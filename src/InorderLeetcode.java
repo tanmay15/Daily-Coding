c21ded95d08d35b81e84a751a3fb5d3d3f01f095
Tanmay Deshpande <tanmay_deshpande3@persistent.com>
2021-12-04 18:13:34 +0530

    Inorder traversal

---
+++ src/InorderLeetcode.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InorderLeetcode {
+	/**
+	 * Definition for a binary tree node.*/
+	 public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	 }
+	 
+	class Solution {
+	    public List<Integer> inorderTraversal(TreeNode root) {
+	        List list = new ArrayList<Integer>();
+	        inorder(root,list);
+	        return list;
+	    }
+	    
+	    public List<Integer> inorder(TreeNode root, List<Integer> list)
+	    {
+	        if(root==null)
+	            return null;
+	        inorder(root.left,list);
+	        list.add(root.val);
+	        inorder(root.right,list);
+	        return list;
+	    }
+	}
+}
